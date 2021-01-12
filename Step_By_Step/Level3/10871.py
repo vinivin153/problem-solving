d1083ae7741225adc75f60a56a529a29e1e4d48a
vinivin153 <kimhongvin@naver.com>
2021-01-13 05:08:07 +0900

    for

---
+++ Step_By_Step/Level3/10871.py
@@ -0,0 +1,5 @@
+a, b = map(int, input().split())
+arr = list(map(int, input().split()))
+for i in range(a):
+    if arr[i] < b:
+        print(arr[i], end=" ")
---
+++ Step_By_Step/Level3/2439.py
@@ -0,0 +1,3 @@
+a = int(input()) + 1
+for i in range(1, a):
+    print(" " * (a - i - 1) + "*" * i)
