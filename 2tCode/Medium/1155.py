f6e7da1cdbe84f24c02fb5a9d9471f5a23eb5747
vinivin153 <kimhongvin@naver.com>
2021-01-15 03:38:06 +0900

    Algorithm - DP Distinct Ways(★★★)

--- 2tCode/Medium/1155.py
+++ 2tCode/Medium/1155.py
@@ -1,15 +1,15 @@
 class Solution:
     def numRollsToTarget(self, d: int, f: int, target: int) -> int:
-        arr = [["0" for i in range(target + 1)] for j in range(d)]
+        arr = [[0 for i in range(target + 1)] for j in range(d)]
 
-        for i in range(1, f) if target >= f else range(1, target):
+        for i in range(1, f + 1) if target >= f else range(1, target + 1):
             arr[0][i] = 1
 
         for i in range(1, d):
             for j in range(target + 1):
-                if target - f <= 0:
+                if j - f <= 0:
                     arr[i][j] = sum(arr[i - 1][:j])
-                else:
-                    arr[i][j] = sum(arr[i - 1][target - f : j])
+                elif j - f > 0:
+                    arr[i][j] = sum(arr[i - 1][j - f : j])
 
-        print(arr[d - 1][target])
+        return arr[d - 1][target] % (10 ** 9 + 7)
