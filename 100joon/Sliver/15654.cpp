da385b05bfae26665d2012c2b95e81a98e3864bd
vinivin153 <vinivin153@naver.com>
2022-02-11 15:44:16 +0900

    backtracking⭐

---
+++ 100joon/Sliver/15654.cpp
@@ -0,0 +1,53 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+
+using namespace std;
+
+int n, m;
+vector<int> arr;
+vector<int> s;
+bool visited[10001];
+
+void seq(int cnt)
+{
+    if (cnt == m)
+    {
+        for (auto &i : s)
+            cout << i << ' ';
+        cout << '\n';
+        return;
+    }
+
+    for (int i = 0; i < n; i++)
+    {
+        if (!visited[arr[i]])
+        {
+            visited[arr[i]] = 1;
+            s.push_back(arr[i]);
+            seq(cnt + 1);
+            visited[arr[i]] = 0;
+            s.pop_back();
+        }
+    }
+}
+
+int main()
+{
+    iostream::sync_with_stdio(0);
+    cin.tie(nullptr);
+    cout.tie(nullptr);
+
+    cin >> n >> m;
+
+    int tmp;
+    for (int i = 0; i < n; i++)
+    {
+        cin >> tmp;
+        arr.push_back(tmp);
+    }
+
+    sort(arr.begin(), arr.end());
+    seq(0);
+    return 0;
+}
