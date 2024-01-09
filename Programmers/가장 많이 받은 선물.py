465e65532aaf9ef2f5c00ae76fe268e871948716
vinivin153 <vinivin153@naver.com>
2024-01-09 22:27:18 +0900

    implement - ⭐️

---
+++ Programmers/가장 많이 받은 선물.py
@@ -0,0 +1,33 @@
+from collections import defaultdict
+from itertools import combinations
+
+
+def solution(friends, gifts):
+    gscore = defaultdict(int)
+    glog = {friend: defaultdict(int) for friend in friends}
+
+    for gift in gifts:
+        give, take = gift.split()
+        gscore[give] += 1
+        gscore[take] -= 1
+        glog[give][take] += 1
+
+    answer = defaultdict(int)
+    for i, j in combinations(friends, 2):
+        if i == j:
+            continue
+
+        if glog[i][j] > glog[j][i]:
+            answer[i] += 1
+        elif glog[i][j] < glog[j][i]:
+            answer[j] += 1
+        else:
+            if gscore[i] > gscore[j]:
+                answer[i] += 1
+            elif gscore[i] < gscore[j]:
+                answer[j] += 1
+
+    if answer:
+        return max(answer.values())
+    else:
+        return 0
