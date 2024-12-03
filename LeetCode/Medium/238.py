65d725f3ebaa142a98677b46d2436006186bb4dd
vinivin153 <vinivin153@naver.com>
2024-12-03 15:59:59 +0900

    array - ⭐️(🟡)

---
+++ LeetCode/Medium/238.py
@@ -0,0 +1,16 @@
+class Solution:
+    def productExceptSelf(self, nums: List[int]) -> List[int]:
+        n = len(nums)
+        ans = [1] * n
+
+        curr = 1
+        for i in range(n):
+            ans[i] = curr
+            curr *= nums[i]
+
+        curr = 1
+        for i in range(n - 1, -1, -1):
+            ans[i] *= curr
+            curr *= nums[i]
+
+        return ans
